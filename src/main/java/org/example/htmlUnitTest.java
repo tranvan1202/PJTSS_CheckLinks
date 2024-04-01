caa23ab090e0a2ace9adc599e6688cb220d6b239
Trần Văn <32728582+tranvan1202@users.noreply.github.com>
2024-04-01 17:24:53 +0700

    updated 10

--- src/main/java/org/example/htmlUnitTest.java
+++ src/main/java/org/example/htmlUnitTest.java
@@ -1,22 +1,25 @@
 package org.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class htmlUnitTest {
     public static void main(String[] args) {
-        // Creating a new instance of the HTML unit driver
-
-        WebDriver driver = new HtmlUnitDriver();
-
-        // Navigate to Google
-        driver.get("http://www.samsung.com/sg");
-
-        // This code will print the page title
-        System.out.println("Page title is: " + driver.getTitle());
-
-        driver.quit();
+        try {
+            Document doc = Jsoup.connect("https://samsung.com/ph/offer/online/2024/galaxy-week/").get();
+            Elements paragraphs = doc.select("div.feature-column-carousel__sub-title");
+            for(Element p : paragraphs) {
+                System.out.println(p.text());
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
+
 }
